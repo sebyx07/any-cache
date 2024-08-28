a17b39e3084fbf471d9ee1ee60954c433e0b406c
sebi <gore.sebyx@yahoo.com>
2024-08-28 14:33:30 +0100

    init

---
+++ lib/any-cache.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Dir.glob(File.join(__dir__, 'any-cache', '**', '*.rb')).sort.each do |file|
+  require file
+end
+
+module AnyCache
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/any-cache/base_cache.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module AnyCache
+  class BaseCache
+    def initialize
+      @cache = {}
+    end
+  end
+end
